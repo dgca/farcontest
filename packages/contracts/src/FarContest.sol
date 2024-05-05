8768af0fa4036bb4383d96e9509ef1c895524a1e
Daniel Cortes <danielgcortes@gmail.com>
2024-05-05 09:18:55 -0700

    Last push?

--- packages/contracts/src/FarContest.sol
+++ packages/contracts/src/FarContest.sol
@@ -6,19 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FarContest is ERC721, ERC721URIStorage, Ownable {
-    enum RewardModel {
-        Tiered,
-        Flat
-    }
-
     struct Contest {
         address creator;
         string name;
-        string description;
         uint256 winnersCount;
-        RewardModel rewardModel;
         address[] winners;
-        uint256 endDateTime;
     }
 
     uint256 internal nextId = 1;
@@ -31,30 +23,25 @@ contract FarContest is ERC721, ERC721URIStorage, Ownable {
 
     function createContest(
         string calldata _name,
-        string calldata _description,
-        uint256 _winnersCount,
-        RewardModel _rewardModel,
-        uint256 _endDateTime
+        uint256 _winnersCount
     ) external payable {
         Contest memory contest = Contest({
             creator: msg.sender,
             name: _name,
-            description: _description,
             winnersCount: _winnersCount,
-            rewardModel: _rewardModel,
-            winners: new address[](_winnersCount),
-            endDateTime: _endDateTime
+            winners: new address[](_winnersCount)
         });
         contests.push(contest);
+        _safeMint(msg.sender, contests.length);
     }
 
-    function safeMint(
-        address to,
-        uint256 tokenId,
-        string memory uri
-    ) public payable onlyOwner {
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    function setWinner(uint256 _contestId, address _winner) external onlyOwner {
+        Contest storage contest = contests[_contestId];
+        require(
+            contest.winners.length < contest.winnersCount,
+            "All winners are already added"
+        );
+        contest.winners.push(_winner);
     }
 
     function withdraw() public onlyOwner {
